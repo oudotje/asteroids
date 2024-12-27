d97a94e9405bfbb19aff924b0b1b3c6e40380aca
oudotje <jean.oudot.06@hotmail.com>
2024-12-27 16:33:38 +0700

    Update the game loop to close the screen

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while(True):
-        for even in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
